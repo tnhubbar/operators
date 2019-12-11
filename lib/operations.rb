961eeacb88349784593753f7f3b1b9536b9fdf87
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-11 00:51:27 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,22 @@
-def unsafe?(speed)
+#The first method, `unsafe?` will take in an argument of a speed and return true if the speed is unsafe and false if it is safe. 
+#   * Use an `if`/`else` statement pair to build the `unsafe?` method. It should return `true` if the speed is either below `40` or above `60`. *Going 30 mph on the freeway would be unsafe, as would going 95 mph. Going 50 miles per hour, however, would return `false` as that's within the "safe" range.*
+# 4.  Build the method `not_safe?` that is a version of your previous `unsafe?` method but use the ternary operator (`?:`) instead of an `if`/`else` statement pair.
 
+
+def unsafe?(speed)
+  if speed < 40 || speed > 60
+    return true 
+  else 
+    return false 
+end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40? return "true" : return "false" 
 end
+
 	
 
 
